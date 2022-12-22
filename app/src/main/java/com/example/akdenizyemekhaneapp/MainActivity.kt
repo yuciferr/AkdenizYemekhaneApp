8fa6f7c87b20ca9570879d41558c70f2e4184829
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2022-12-23 01:07:00 +0300

    Home screen resource added

--- app/src/main/java/com/example/akdenizyemekhaneapp/MainActivity.kt
+++ app/src/main/java/com/example/akdenizyemekhaneapp/MainActivity.kt
@@ -6,12 +6,17 @@ import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.*
 import androidx.navigation.ui.setupWithNavController
+import com.example.akdenizyemekhaneapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val navController = findNavController(R.id.fragment)
@@ -23,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         //disable night mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
+        //hide bottom navigation bar
         navController.addOnDestinationChangedListener{ _, destination, _ ->
             if(destination.id == R.id.loginFragment) {
 
--- app/src/main/java/com/example/akdenizyemekhaneapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/akdenizyemekhaneapp/ui/home/HomeFragment.kt
@@ -1,10 +1,62 @@
 package com.example.akdenizyemekhaneapp.ui.home
 
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.akdenizyemekhaneapp.R
+import com.example.akdenizyemekhaneapp.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var homeAdapter: HomeRecyclerAdapter
+    private lateinit var homeItems: ArrayList<HomeItem>
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Home Recycler View
+        binding.homeRecyclerView.setHasFixedSize(true)
+
+        homeItems = ArrayList()
+        homeItems.add(HomeItem("Monday", "Kuru Fasulye", R.drawable.kurufasulye,
+            "Pirinç Pilavı", R.drawable.pilav, "Mevsim Salatası",
+            R.drawable.salata, "Ayran", R.drawable.ayran))
+
+        homeItems.add(HomeItem("Tuesday", "Mercimek Çorbası", R.drawable.mercimek,
+            "Kıymalı Makarna", R.drawable.makarna, "Tulumba Tatlısı",
+            R.drawable.tulumba, "Yoğurt", R.drawable.yogurt))
+
+        homeItems.add(HomeItem("Wednesday", "Rosto Köfte", R.drawable.kofte,
+            "Bulgur Pilavı", R.drawable.bulgur, "Tahinli Piyaz",
+            R.drawable.piyaz, "Ayran", R.drawable.ayran))
+
+        homeItems.add(HomeItem("Thursday", "Etli Nohut", R.drawable.nohut,
+            "Pirinç Pilavı", R.drawable.pilav, "Mevsim Salatası",
+            R.drawable.salata, "Tulumba Tatlısı", R.drawable.tulumba))
+
+        homeItems.add(HomeItem("Friday", "Mercimek çorbası", R.drawable.mercimek,
+            "Kıymalı Makarna", R.drawable.makarna, "Kuru Fasulye",
+            R.drawable.kurufasulye, "Ayran", R.drawable.ayran))
+
+        homeAdapter = HomeRecyclerAdapter(homeItems)
+        binding.homeRecyclerView.adapter = homeAdapter
+        binding.homeRecyclerView.layoutManager = LinearLayoutManager(context)
+
+    }
 
 }
