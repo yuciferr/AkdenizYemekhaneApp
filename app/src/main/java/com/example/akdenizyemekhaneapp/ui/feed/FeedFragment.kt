98fff3d30a1a1c9a5aa0fa54ea3bb4efec7c2890
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2022-12-23 01:52:21 +0300

    Feed page recycler adapter was set up

--- app/src/main/java/com/example/akdenizyemekhaneapp/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/example/akdenizyemekhaneapp/ui/feed/FeedFragment.kt
@@ -5,7 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.akdenizyemekhaneapp.R
-class FeedFragment : Fragment(R.layout.fragment_feed) {
+import com.example.akdenizyemekhaneapp.databinding.FragmentFeedBinding
+
+class FeedFragment : Fragment() {
+
+    private lateinit var binding: FragmentFeedBinding
+    private lateinit var feedRecyclerAdapter: FeedRecyclerAdapter
+    private lateinit var feedList: List<Feed>
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFeedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.feedRecyclerView.setHasFixedSize(true)
+        feedList = listOf(
+            Feed("Yusuf Samed Çelik", "Akdeniz Üniversitesi yemekhane yemekleri çok güzel", "5.0"),
+            Feed("Ulaş Gergerli", "Akdeniz Üniversitesi yemekhane yemekleri çok güzel", "4.0"),
+            Feed("Emre Yaşar", "Akdeniz Üniversitesi yemekhane yemekleri çok güzel", "4.5")
+        )
+
+        feedRecyclerAdapter = FeedRecyclerAdapter(feedList)
+        binding.feedRecyclerView.adapter = feedRecyclerAdapter
+        binding.feedRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
 
 }
--- app/src/main/java/com/example/akdenizyemekhaneapp/ui/feed/FeedRecyclerAdapter.kt
+++ app/src/main/java/com/example/akdenizyemekhaneapp/ui/feed/FeedRecyclerAdapter.kt
@@ -1,4 +1,31 @@
 package com.example.akdenizyemekhaneapp.ui.feed
 
-class FeedRecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.akdenizyemekhaneapp.databinding.RvFeedItemBinding
+
+class FeedRecyclerAdapter(private val feedList: List<Feed>):
+    RecyclerView.Adapter<FeedRecyclerAdapter.ViewHolder>() {
+
+    inner class ViewHolder(internal val binding: RvFeedItemBinding): RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(RvFeedItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.apply{
+            feedTitle.text = feedList[position].feedTitle
+            feedComment.text = feedList[position].feedComment
+            feedRate.text = feedList[position].feedRate
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+       return feedList.size
+    }
 }
